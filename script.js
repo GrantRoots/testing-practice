b07c706314f5a8a68428e48f9969a56f7d32520b
GrantRoots <158632641+GrantRoots@users.noreply.github.com>
2024-11-21 20:16:28 -0700

    feat: Make caesarCipher()

--- script.js
+++ script.js
@@ -21,4 +21,41 @@ const calculator = {
     }
 }
 
-export { capitalize, reverseString, calculator }
+function caesarCipher(string, shiftFactor) {
+    //get ascii values
+    function isLetter(char) {
+        const charCode = char.charCodeAt(0);
+        if ((charCode >= 65 && charCode <= 90) || (charCode >= 97 && charCode <= 122)) {
+            return true
+        }
+        return false
+    }
+    let shiftedString = ''
+    for (let i = 0; i < string.length; i++) {
+        if (isLetter(string[i]) === true) {
+            let asciiValue = string.charCodeAt(i)
+            if (asciiValue >= 65 && asciiValue <= 90) {
+                for (let i = 0; i < shiftFactor; i++) {
+                    if (asciiValue + 1 > 90) {
+                        asciiValue = 64
+                    }
+                    asciiValue += 1
+                }
+                shiftedString += String.fromCharCode(asciiValue)
+            }
+            if (asciiValue >= 97 && asciiValue <= 122) {
+                for (let i = 0; i < shiftFactor; i++) {
+                    if (asciiValue + 1 > 122) {
+                        asciiValue = 96
+                    }
+                    asciiValue += 1
+                }
+                shiftedString += String.fromCharCode(asciiValue)
+            }
+        }
+        else { shiftedString += string[i] }
+    }
+    return shiftedString
+}
+
+export { capitalize, reverseString, calculator, caesarCipher }
--- script.test.js
+++ script.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString, calculator } from './script.js'
+import { capitalize, reverseString, calculator, caesarCipher } from './script.js'
 
 test('capitalize', () => {
     expect(capitalize('apple')).toBe('Apple')
@@ -10,4 +10,8 @@ test('reverse sring', () => {
 
 test('calculator', () => {
     expect(calculator.multiply(1, 3)).toBe(3)
+})
+
+test('Caesar Cipher', () => {
+    expect(caesarCipher('HeLLo', 3)).toBe('KhOOr')
 })
